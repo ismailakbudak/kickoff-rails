0ad16480d5479982ca4984ec1977d63af0700258
ismail <isoakbudak@gmail.com>
2014-05-01 00:23:01 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myapp::Application.routes.draw do
  
-
+  resources :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
