fcdc7e2901eb6e6d34e114da84eb20f2d4e2bb1f
ismail <isoakbudak@gmail.com>
2014-05-05 21:04:25 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new( user_params )    
     if @user.save
+       sign_in @user
        flash[:success] = "Welcome to the Sample App!" # flash message	
        redirect_to @user #user has been saved succesfully
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 	
 	# some databases does not  support case_sensitive
 	before_save { self.email = email.downcase }
+    
+    # remember me için eklendi
+    before_create :create_remember_token
 
 	# it is a method we can use like this => validates(:name, presence: true)
 	validates :name, presence: true, length: { maximum: 50 }   
@@ -11,6 +14,20 @@ class User < ActiveRecord::Base
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, length: { minimum: 6 }
-
     
+    # remember me için eklendi
+    def User.new_remember_token
+	    SecureRandom.urlsafe_base64
+	end
+
+	def User.digest(token)
+	  Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+	    def create_remember_token
+	      self.remember_token = User.digest(User.new_remember_token)
+	    end
+
 end
